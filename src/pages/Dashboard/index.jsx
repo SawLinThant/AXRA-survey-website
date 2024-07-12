e346a9d78099b920f86799200556cc1dc23a03d5
Wai Yan Htet <waiyanhtet1661390@gmail.com>
2024-07-12 15:48:48 +0630

    setup: logout feature

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,17 +1,20 @@
-import CustomSidebar from "@/components/sidebar";
 import { Route, Routes } from "react-router-dom";
+import Header from "./Header";
 import Survey from "./survey";
 import Users from "./user";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-row">
-      <CustomSidebar />
-      <Routes>
-        <Route path="*" element={<Survey />}></Route>
-        <Route path="Users" element={<Users />}></Route>
-      </Routes>
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-row">
+        {/* <CustomSidebar /> */}
+        <Routes>
+          <Route path="*" element={<Survey />}></Route>
+          <Route path="Users" element={<Users />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 };
 export default Dashboard;
