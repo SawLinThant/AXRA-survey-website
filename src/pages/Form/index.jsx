62f0de4b3908fa97c0eef4bf105f39dd7835b842
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-07-16 18:26:16 +0700

    ui fix

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -13,8 +13,8 @@ import ThankYou from "./thankyou";
 const Form = () => {
   return (
     <OptionProvider>
-      <div className="w-full md:w-full lg:w-full h-screen bg-background_color">
-        <div className="w-full min-h-screen rounded-[20px] border-[5px] border-form_border bg-background_color">
+      <div className="w-full md:w-full lg:w-full bg-background_color">
+        <div className="w-full rounded-[20px] border-[5px] border-form_border bg-background_color">
           <Routes>
             <Route path="*" element={<SplashScreen />} />
             <Route path="Selectoptions" element={<SelectOption />} />
